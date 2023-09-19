3f4329ef03c657607cd0bae55d48b1436e333c2b
VIENPC <vanvien2520@gmail.com>
2023-09-19 16:19:49 +0700

    commit one
    cat giao diện người dùng

---
+++ src/main/java/com/nhutin/electric_project/user/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.nhutin.electric_project.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/home")
+    public String index(){
+
+        return "banhang/view/home";
+    }
+     @RequestMapping("/about")
+    public String about(){
+
+        return "banhang/view/about";
+    }
+      @RequestMapping("/shop")
+    public String product(){
+
+        return "banhang/view/product_list";
+    }
+  @RequestMapping("/detail")
+    public String detail(){
+
+        return "banhang/view/product_detail";
+    }
+    @RequestMapping("/cart")
+    public String cart(){
+
+        return "banhang/view/cart";
+    }
+    @RequestMapping("/checkout")
+    public String checkout(){
+
+        return "banhang/view/checkout";
+    }
+}
