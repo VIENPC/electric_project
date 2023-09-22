542799a9486e193cfc6c618e2b637f2fbee9f6b6
VIENPC <vanvien2520@gmail.com>
2023-09-22 14:41:16 +0700

    commit 22

--- src/main/java/com/nhutin/electric_project/user/rest/UserRestController.java
+++ src/main/java/com/nhutin/electric_project/user/rest/UserRestController.java
@@ -1,5 +1,24 @@
 package com.nhutin.electric_project.user.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nhutin.electric_project.model.Category;
+import com.nhutin.electric_project.repository.categorysRepository;
+
+
+@RestController
 public class UserRestController {
-    
+    @Autowired
+    categorysRepository dmdao;
+
+    @GetMapping("/rest/danhmuc")
+	public ResponseEntity<List<Category>> findAll() {
+
+		return ResponseEntity.ok(dmdao.findAll());
+	}
 }
