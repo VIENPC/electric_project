3778a048b8af9fedc22fb0fb5cb7c1ff7c23085f
khoi1233 <114672335+khoi1233@users.noreply.github.com>
2023-09-25 18:05:39 +0700

    đưa dữ liệu lên table

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,43 @@
+const host = "http://localhost:8080/rest";
+const app = angular.module("OrderApp", []);
+app.controller("OrderCtrl", function ($scope, $http) {
+    $scope.listusers = [];
+    $scope.load_all = function () {
+        const url = `${host}/users`;
+        $http.get(url).then((resp) => {
+            $scope.listusers = resp.data;
+            console.log("Sucsess", resp.data);
+            $scope.pageNumber = 1;
+            $scope.limit = 8;
+            $scope.totalPage = getTotalPage($scope.listusers, $scope.limit);
+            
+           
+        })
+    }
+    $scope.setLimit = (soSanPham) => {
+        $scope.pageNumber = 1;
+        $scope.limit = soSanPham;
+        $scope.totalPage = getTotalPage($scope.listusers, $scope.limit);
+    };
+    $scope.setPageNumber = (pageNumber) => {
+        $scope.pageNumber = pageNumber;
+    };
+    const getTotalPage = (arr, soSanPham) => {
+        return Math.ceil(arr.length / soSanPham);
+    };
+    $scope.load_all();
+    $scope.detail = (id) => {
+        // location.replace("./orderdetail.html?idHoaDon=" + id);
+    }
+});
+app.filter('range', function () {
+    return function (input, total) {
+        total = parseInt(total);
+
+        for (var i = 0; i < total; i++) {
+            input.push(i);
+        }
+
+        return input;
+    };
+});
