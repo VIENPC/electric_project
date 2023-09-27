7f868d5d6ab823fa9c105e6a5a36a82abc0099d4
quangminh15 <phamm7696@gmail.com>
2023-09-27 11:41:59 +0700

    Đăng nhập với facebook

--- src/main/java/com/nhutin/electric_project/security/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/nhutin/electric_project/security/handler/OAuth2LoginSuccessHandler.java
@@ -12,10 +12,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.nhutin.electric_project.model.CustomOAuth2User; // Sử dụng CustomOAuth2User thay vì DefaultOidcUser
 import com.nhutin.electric_project.model.User;
 import com.nhutin.electric_project.repository.UserRepository;
 import com.nhutin.electric_project.security.service.UserDetailsServiceImpl;
@@ -35,9 +35,9 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
 
-        DefaultOidcUser oauthUser = (DefaultOidcUser) authentication.getPrincipal();
+        CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal(); // Ép kiểu thành CustomOAuth2User
 
-        String email = oauthUser.getAttribute("email");
+        String email = oauthUser.getEmail(); // Sử dụng getter của CustomOAuth2User
         String name = oauthUser.getFullName();
         String phone = oauthUser.getPhoneNumber();
 
@@ -48,19 +48,15 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
             User user = nguoiDungDAO.findByEmail(email).get();
             userDetails = userService.updateUserAfterOAuthLogin(user, name);
         }
-        session.setAttribute("tenDangNhapLogin", oauthUser.getAttribute("email"));
+        session.setAttribute("tenDangNhapLogin", email); // Sử dụng biến email thay vì oauthUser.getAttribute("email")
         Authentication newAuthentication = new UsernamePasswordAuthenticationToken(
                 userDetails, null, userDetails.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(newAuthentication);
 
-        System.out.println("Cumstomer's Email: " + email);
+        System.out.println("Customer's Email: " + email);
 
-//        super.onAuthenticationSuccess(request, response, authentication);
-        
         super.setDefaultTargetUrl("/home");
         super.onAuthenticationSuccess(request, response, authentication);
-
     }
 }
-
