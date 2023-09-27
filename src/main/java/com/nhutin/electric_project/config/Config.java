59615b2526d811c46494f26e23651552225a364f
quangminh15 <phamm7696@gmail.com>
2023-09-27 13:54:13 +0700

    Đổi port

--- src/main/java/com/nhutin/electric_project/config/Config.java
+++ src/main/java/com/nhutin/electric_project/config/Config.java
@@ -19,11 +19,4 @@ public class Config implements WebMvcConfigurer{
                 .addResourceLocations("classpath:/static/", "classpath:/templates/")
                 ;
     }
-	
-//	 @Override
-//	    public void addInterceptors(InterceptorRegistry registry) {
-//	        registry.addInterceptor(myInterceptor)
-//	                .addPathPatterns("/**") // Đường dẫn để áp dụng interceptor, bạn có thể chỉ định rõ các URL
-//	                .excludePathPatterns("/assets/**"); // Không chặn những đường dẫn chứa tài nguyên (css, js, ...)
-//	    }
 }
