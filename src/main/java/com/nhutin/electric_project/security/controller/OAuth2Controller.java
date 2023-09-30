a924266d5507d07cc432f55620ef019ebef1d5e0
quangminh15 <phamm7696@gmail.com>
2023-10-01 00:34:45 +0700

    đăng nhập thành công bằng fb và gg
    - Lưu đc dữ liệu vào db

--- src/main/java/com/nhutin/electric_project/security/controller/OAuth2Controller.java
+++ src/main/java/com/nhutin/electric_project/security/controller/OAuth2Controller.java
@@ -9,6 +9,7 @@ public class OAuth2Controller {
 
     @GetMapping("/loginSuccess")
     public String loginSuccess(OAuth2AuthenticationToken authentication) {
+    	System.out.println(authentication);
         // Xử lý logic sau khi đăng nhập thành công
         return "redirect:/home";
     }
