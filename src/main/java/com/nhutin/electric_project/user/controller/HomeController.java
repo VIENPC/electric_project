58a40cca5b367b348862230abc2c3bf69e51e8e8
khoi1233 <114672335+khoi1233@users.noreply.github.com>
2023-10-09 13:39:09 +0700

    khuyenmai

--- src/main/java/com/nhutin/electric_project/user/controller/HomeController.java
+++ src/main/java/com/nhutin/electric_project/user/controller/HomeController.java
@@ -1,49 +1,36 @@
 package com.nhutin.electric_project.user.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.nhutin.electric_project.config.CookieUtils;
-import com.nhutin.electric_project.model.Comment;
-import com.nhutin.electric_project.model.Product;
-import com.nhutin.electric_project.model.User;
-import com.nhutin.electric_project.repository.CommentsDao;
-import com.nhutin.electric_project.repository.UserRepository;
-import com.nhutin.electric_project.repository.productsRepository;
+import com.nhutin.electric_project.model.Order;
+import com.nhutin.electric_project.model.Promotion;
+import com.nhutin.electric_project.repository.ordersRepository;
+import com.nhutin.electric_project.repository.prmotionRepositpry;
+
 
 @Controller
 public class HomeController {
-    @Autowired
+ @Autowired
     HttpSession session;
-
-    @Autowired
-    CommentsDao cmdao;
-
-    @Autowired
-    productsRepository prdao;
-
-    @Autowired
-    CookieUtils cook;
-    @Autowired
-    UserRepository userDAO;
-
+@Autowired
+    ordersRepository orderDao;
+@Autowired
+prmotionRepositpry prmoDao;
     @RequestMapping("/home")
     public String index() {
 
         return "banhang/view/home";
     }
-
-    @RequestMapping("/success")
+     @RequestMapping("/success")
     public String thongbao() {
 
         return "banhang/view/sucessful";
@@ -57,29 +44,29 @@ public class HomeController {
 
     // @RequestMapping("/shop")
     // public String product() {
-    // return "banhang/view/product_list";
+    //     return "banhang/view/product_list";
     // }
 
     // @RequestMapping("/shop/{categoryID}")
     // public String productCategory() {
-    // return "banhang/view/product_list";
+    //     return "banhang/view/product_list";
     // }
-    @RequestMapping("/shop")
-    public String productCategory(@RequestParam(name = "categoryID", required = false) Integer categoryID,
-            @RequestParam(name = "brandID", required = false) Integer brandID,
-            @RequestParam(name = "_fragment", required = false) Integer fragmentID) {
-        if (fragmentID != null) {
-            // Sử dụng giá trị của fragmentID để điều hướng đến trang giao diện
-            if (categoryID != null) {
-                // Xử lý categoryID nếu cần
-            } else if (brandID != null) {
-                // Xử lý brandID nếu cần
-            }
-            return "banhang/view/product_list";
-        } else {
-            return "banhang/view/product_list"; // Trường hợp mặc định
+@RequestMapping("/shop")
+public String productCategory(@RequestParam(name = "categoryID", required = false) Integer categoryID,
+                              @RequestParam(name = "brandID", required = false) Integer brandID,
+                              @RequestParam(name = "_fragment", required = false) Integer fragmentID) {
+    if (fragmentID != null) {
+        // Sử dụng giá trị của fragmentID để điều hướng đến trang giao diện
+        if (categoryID != null) {
+            // Xử lý categoryID nếu cần
+        } else if (brandID != null) {
+            // Xử lý brandID nếu cần
         }
+        return "banhang/view/product_list";
+    } else {
+        return "banhang/view/product_list"; // Trường hợp mặc định
     }
+}
 
     // @RequestMapping("/shop/{brandID}")
     // public String productBrand() {
@@ -87,31 +74,7 @@ public class HomeController {
     // }
 
     @RequestMapping("/detail/{productID}")
-    public String detailid(@PathVariable("productID") int productID, Model model) {
-
-        List<Comment> com = cmdao.findCommentByProductId(productID);
-
-        for (Comment list : com) {
-            System.out.println(list.getContent());
-        }
-        model.addAttribute("listcomment", com);
-        return "banhang/view/product_detail";
-    }
-
-    @RequestMapping("comment")
-    public String comment(HttpServletRequest req) {
-        String email = cook.get("tenDangNhapCookie", req);
-
-        User kh = userDAO.findByEmailLike(email);
-        Comment comment = new Comment();
-
-        Product product = prdao.findById(Integer.parseInt(req.getParameter("productid")));
-        comment.setProduct(product);
-        comment.setUser(kh);
-        comment.setContent(req.getParameter("contect"));
-
-        cmdao.save(comment);
-
+    public String detailid() {
         return "banhang/view/product_detail";
     }
 
@@ -127,12 +90,11 @@ public class HomeController {
     }
 
     @RequestMapping("/checkout")
-    public String checkout(HttpServletRequest req, Model model) {
-        String email = cook.get("tenDangNhapCookie", req);
-
-        User kh = userDAO.findByEmailLike(email);
-        model.addAttribute("account", kh);
-        return "banhang/view/checkout";
+    public String checkout(Model model) {  
+		   
+        List<Promotion> kmlist = prmoDao.findAll();	      
+        model.addAttribute("items", kmlist);
+         return "banhang/view/checkout";
     }
 
     @RequestMapping("/news")
